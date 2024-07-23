96baf6cdbbc7f29718532303d768e2ef8f319609
Ashu09 <asheeshyadav103@gmail.com>
2024-07-23 22:31:18 +0530

    PPDC D2

---
+++ D2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float c,f;
+    cout<<"Enter the celsius : ";
+    cin>>c;
+    f=c*(1.8)+32;
+    cout<<c<<" Celcius is Equal to "<<f<<" Fahrenheit "<<endl;
+    return 0;
+}
