8df4d62e38790486421bae7b6001b754a535e1c0
ashucodx <ashucodx@gmail.com>
2024-07-25 07:55:44 +0530

    PPDC D6extra2

---
+++ D6extra2.cpp
@@ -0,0 +1,11 @@
+//Function without Parameters and Return Type
+#include<iostream>
+using namespace std;
+void greet();
+int main(void){
+    greet();
+    return 0;
+}
+void greet(){
+    cout<<"hello Coders!"<<endl;
+}
