0551d6a4ac131bd71ffd74f83a55de2435736b34
ashucodx <ashucodx@gmail.com>
2024-07-28 22:01:47 +0530

    PPDC D8 extra2

---
+++ D8extra2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Char: "<<sizeof(char)<<endl;
+    cout<<"int: "<<sizeof(int)<<endl;
+    cout<<"float: "<<sizeof(float)<<endl;
+    cout<<"double: "<<sizeof(double)<<endl;
+    cout<<"bool: "<<sizeof(bool)<<endl;
+}
